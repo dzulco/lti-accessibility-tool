50dd5a750744fafb99d777cf0d4979f551e1618c
Damian Zulcovsky <damez77@gmail.com>
2026-08-03 01:38:56 -0300

    add index page

---
+++ src/main/java/com/innovalab/ltitool/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.innovalab.ltitool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // Esto buscará src/main/resources/templates/index.html
+    }
+}
